0d9e397f6f191ef1b904b7f38ec35003f51f1dac
BangKiHyun <rlrlvh@naver.com>
2020-06-07 14:44:00 +0900

    ch06-model3에서 요구사항 추가(상속)

---
+++ ch07-model4/src/main/java/jpabook/entity/Category.java
@@ -0,0 +1,51 @@
+package jpabook.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "CATEGORY_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<CategoryItem> categoryItems = new ArrayList<CategoryItem>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CategoryItem> getCategoryItems() {
+        return categoryItems;
+    }
+
+    public void setCategoryItems(List<CategoryItem> categoryItems) {
+        this.categoryItems = categoryItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
